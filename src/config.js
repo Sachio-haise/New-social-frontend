3d8d7aba70dc1a55243c9b26a6113197d1a30a96
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2022-07-18 20:05:25 +0630

    6th commit

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const server_url = "http://localhost:5000";
+export const server_url = "https://server-social2022.herokuapp.com";
 //https://mern-social-app-2022.herokuapp.com
+//https://server-social2022.herokuapp.com/
 export const Access_Control_Allow_Origin = "*";
