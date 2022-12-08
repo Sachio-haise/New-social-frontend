503c6d813ed005e13a8db42debbe74812317ee46
Haruto <66941759+Sachio-haise@users.noreply.github.com>
2022-12-08 04:08:44 -0500

    Update api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-export const server_url = "https://server-social2022.herokuapp.com";
+export const server_url = "https://new-social-test.vercel.app";
 //http://localhost:5000
 //https://mern-social-app-2022.herokuapp.com
 //https://server-social2022.herokuapp.com/
+//https://new-social-test.vercel.app/
 export const Access_Control_Allow_Origin = "*";
