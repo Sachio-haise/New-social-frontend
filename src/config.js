009f4c8d775ace45ca9bc6b0a2f5b779b7b8b791
Haruto <66941759+Sachio-haise@users.noreply.github.com>
2023-02-01 15:29:26 +0630

    Update url api 2023

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const server_url = "https://new-social-test.vercel.app";
+export const server_url = "https://socail-app-2022-backend.onrender.com";
+//https://new-social-test.vercel.app
 //http://localhost:5000
 //https://mern-social-app-2022.herokuapp.com
 //https://server-social2022.herokuapp.com/
